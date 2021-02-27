28d8e2025d9f6a42687501fade9a981b177560f6
Darshilp326 <darshil_1901me18@iitp.ac.in>
2021-02-27 12:15:06 +0530

    Merge conflicts

--- client/components/FilterByText.tsx
+++ client/components/FilterByText.tsx
@@ -38,11 +38,7 @@ const FilterByText: FC<FilterByTextProps> = ({
 	}, []);
 
 	return <Box mb='x16' is='form' onSubmit={handleFormSubmit} display='flex' flexDirection='row' {...props}>
-<<<<<<< HEAD
-		<TextInput placeholder={placeholder ?? t('Search')} autoFocus={true} addon={<Icon name='magnifier' size='x20'/>} onChange={handleInputChange} value={text} />
-=======
 		<TextInput placeholder={placeholder ?? t('Search')} ref={inputRef} addon={<Icon name='magnifier' size='x20'/>} onChange={handleInputChange} value={text} />
->>>>>>> 8a5c5f3971059116222a0abb2646b4dfe628c96f
 		<Button onClick={onButtonClick} display={display ? 'block' : 'none'} mis='x8' primary>{textButton}</Button>
 	</Box>;
 };
